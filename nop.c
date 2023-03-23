fb5785e3ee0c7505c0189e275400799dbfd41255
vinsky001 <ekibet544@gmail.com>
2023-03-23 06:07:46 -0700

    Monty files

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * f_nop- nothing
+ * @head: stack head
+ * @counter: line_number
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
